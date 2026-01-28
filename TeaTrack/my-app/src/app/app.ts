c3ccbb4fb2f345d6ea31480b5df8a5ff44fd4dce
BaoVy2810 <vytnb234112e@st.uel.edu.vn>
2026-01-28 19:56:15 +0700

    update  header  (not done yet)

--- TeaTrack/my-app/src/app/app.ts
+++ TeaTrack/my-app/src/app/app.ts
@@ -1,18 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PageHeader } from '../components/page-header/page-header'; // 👈 thêm dòng này
+import { PageHeader } from './Components/page-header/page-header';
 
 @Component({
   selector: 'app-root',
-  imports: 
-  [
+  standalone: true,
+  imports: [
     RouterOutlet,
     PageHeader
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
-  
 })
 export class App {
-  protected readonly title = signal('my-app');
+  protected readonly title = signal('TeaTrack');
+
 }
+
